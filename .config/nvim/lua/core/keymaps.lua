e9424d134c5bdd5440d8b7b8667f72e3e7496052
rein <rein@inuits.eu>
2023-01-27 11:34:03 +0100

    add nvim lua configs

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,56 @@
+local g = vim.g
+local o = vim.o
+local opt = vim.opt
+
+-- Decrease update time
+o.timeoutlen = 500
+o.updatetime = 200
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 8
+
+-- Better editor UI
+o.number = true
+o.numberwidth = 2
+o.relativenumber = true
+
+-- Better editing experience
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+o.autoindent = true
+o.wrap = true
+o.textwidth = 300
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.list = true
+o.listchars = "trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂"
+
+-- Makes neovim and host OS clipboard play nicely with each other
+o.clipboard = "unnamedplus"
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+
+-- Remember 50 items in commandline history
+o.history = 50
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+opt.mouse = "a"
+
+-- Map <leader> to space
+g.mapleader = " "
+g.maplocalleader = " "
+
+
--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -2,11 +2,11 @@
 alias ll="exa -lh"
 alias lla="exa -lha"
 alias dns="cat /etc/resolv.conf | grep nameserver"
-alias cat='bat --style grid'
-alias dust='du -sh * | sort -hr'
-alias ..='cd ..'
+alias cat="bat --style grid"
+alias dust="du -sh * | sort -hr"
+alias ..="cd .."
+alias vim="nvim"
 
 # Git
-alias gitup='git pull --rebase; git submodule --quiet sync; git submodule update --init'
-alias gitmass='/bin/bash /home/rein/Documents/hulp/scripts/gitmass.sh'
+alias gitup="git pull --rebase; git submodule --quiet sync; git submodule update --init"
 
