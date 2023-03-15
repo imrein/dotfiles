4c30000c81038fc69111fee9aa9dbf761815828c
rein <rein@inuits.eu>
2023-03-15 14:58:29 +0100

    changed code to vscodium

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -5,7 +5,9 @@ alias dns="cat /etc/resolv.conf | grep nameserver"
 alias cat="bat --style grid"
 alias dust="du -sh * | sort -hr"
 alias ..="cd .."
+
 alias vim="nvim"
+alias code="vscodium"
 
 # Git
 alias gitup="git pull --rebase; git submodule --quiet sync; git submodule update --init"
