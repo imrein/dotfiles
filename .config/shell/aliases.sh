b528627d373c808ab6dcec09c9d0b19e5ff8062e
rein <rein@inuits.eu>
2023-08-10 15:35:16 +0200

    small alias

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -6,9 +6,12 @@ alias cat="bat --style grid"
 alias dust="du -sh * | sort -hr"
 alias ..="cd .."
 
+alias ippub="curl https://ipv4.wtfismyip.com/text"
+
+# Editors
 alias vim="nvim"
 alias code="vscodium"
 
 # Git
 alias gitup="git pull --rebase; git submodule --quiet sync; git submodule update --init"
-
+alias gitmass="for dir in *; do if [ -d $dir/.git ]; then cd $dir && git pull --rebase; git submodule --quiet sync; git submodule update --init && cd ..; fi; done"
