6eba4d2fb97f450013de38f312256b997ee70d39
rein <rein@inuits.eu>
2023-08-26 00:19:31 +0200

    removed help shizzle

--- .config/nvim/lua/core/plugin_config/treesitter.lua
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python", "lua", "vim", "help", "markdown", "bash" },
+  ensure_installed = { "python", "lua", "vim", "markdown", "bash" },
 
   sync_install = false,
   auto_install = true,
